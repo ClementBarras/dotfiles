a00a43b06e4825146ba55b9074d38eb8e5725c98
Clement Barras <25439622+ClementBarras@users.noreply.github.com>
2023-08-31 15:08:36 +0900

    Fix zsh: do not autoselect the first completion entry

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -41,10 +41,10 @@ compdef vman="man"
 # | Options |
 # +---------+
 
-# setopt GLOB_COMPLETE      # Show autocompletion menu with globs
-setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
+setopt GLOB_COMPLETE      # Show autocompletion menu with globs
 setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
 setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
+setopt nomenucomplete # Do not autoselect the first completion entry.
 
 # +---------+
 # | zstyles |
